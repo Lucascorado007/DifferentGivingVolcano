87e6b0b5f327e8a7239638f7a42f6059fc85e4c9
Lucas <130715175+Lucascorado007@users.noreply.github.com>
2025-02-26 23:57:10 +0000

    teste2

---
+++ script.js
@@ -0,0 +1,15 @@
+function somar(){
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  let resultado = parseInt(num1) + parseInt(num2);
+  //alert(resultado);
+  console.log(num1)
+}
+function subtracao () {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  let resultado = parseInt(num1) - parseInt(num2);
+  alert(resultado);
+  console.log(num1)
+  
+}
